aa552e0dd8d4baa20a768adbe075395cc103a54f
eHsuan <ac131151@gmail.com>
2025-08-20 14:12:24 +0800

    push test

---
+++ SecsTest.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SecsTest;
+
+namespace SecsTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string rns = StringTest.CompareString("aaa", "BBB");
+            Assert.AreEqual("aaaBBB", rns);
+        }
+    }
+}
---
+++ SecsTest/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecsTest
+{
+    public static class StringTest
+    {
+        public static string CompareString(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
